da159c9f64647dc00c649ffa935803f49e04ee68
Tisha Murvihill <tmurv@shaw.ca>
2024-06-16 19:45:36 -0600

    user endpoints working

--- src/controllers/users/del.ts
+++ src/controllers/users/del.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
-export const del = (req: Request, res: Response) => {
-  res.status(204).send("Delete route working. Function NYI");
+import { User } from "../../models/user-schema";
+
+export const del = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await User.findOneAndDelete({ id });
+
+  res.status(204).json("Delete route working. Function NYI");
 };
